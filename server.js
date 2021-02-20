4a223db877d960180bf69aa96929e85a239bba05
Chris Athanas <chris@crowdskout.com>
2021-02-20 17:47:48 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+});
+
+app.listen(process.env.PORT ?? 3001, () => console.log(`Server running on port:${process.env.PORT ?? 3001}`))
