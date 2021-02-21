d0e6bf0754f1cef7ad5ba2001fc2b8d3ff88e167
Chris Athanas <chris@crowdskout.com>
2021-02-21 02:01:42 -0600

    simplify routing

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -6,8 +6,7 @@ import Settings from "./views/Settings.js";
 const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$"); // match the first "/", get the id
 
 const getParams = match => {
-
-  return pathsToParams(match.result[0], match.route.path)
+  return pathsToParams(match.result[0], match.path)
 
   // const values = match.result.slice(1); // ["/posts/2/4/6","2","4","6"] -> [2,4,6]
   // const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]); // "posts/:id/:a/:b" -> ["id","a","b"]
@@ -27,18 +26,35 @@ const router = async () => {
     { path: "/settings",    view: Settings }
   ]
 
-  // generate regex for each path
-  const potentialMatches = routes.map( route=> {
-    console.log(route.path, pathToRegex(route.path) , location.pathname.match(pathToRegex(route.path)))
-    return {
-      route: route,
-      result: location.pathname.match(pathToRegex(route.path))
-    }
+  // // generate regex for each path
+  // console.log(location.pathname)
+  // const potentialMatches = routes.map( route=> {
+  //   console.log(route.path, pathToRegex(route.path) , location.pathname.match(pathToRegex(route.path)))
+  //   return {
+  //     route: route,
+  //     result: location.pathname.match(pathToRegex(route.path))
+  //   }
+  // })
+
+
+  // // Create the list of potential matching routes
+  console.log("location.pathname=" + location.pathname.split("/")[1])
+  // let potentialMatches = routes.map( route => {
+  //   console.log("route.path=" + route.path.split("/")[1])
+  //   return { 
+  //     route,
+  //     result: location.pathname.split("/")[1] == route.path.split("/")[1]
+  //   }
+  // })
+  
+  // // Find the path that matches
+  // let match = potentialMatches.find(potentialMatch => potentialMatch.result != null)
+
+  let match = routes.find( route => {
+    console.log("route.path='"+route.path.split("/")[1]+"', location.pathname="+location.pathname.split("/")[1])
+    console.log(route.path.split("/")[1] === location.pathname.split("/")[1])
+    return route.path.split("/")[1] === location.pathname.split("/")[1]
   })
-
-
-  // Find the path that matches
-  let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null)
 
   // 404 route
   if (!match) {
@@ -46,11 +62,14 @@ const router = async () => {
       route: routes[0],  // Dashboard.js route is 404
       result: [location.pathname]
     }
+  } else {
+    console.log("match="+match.path)
+    match.result = [location.pathname]
   }
 
   // Load the new view
   console.log("getParams=" + JSON.stringify(getParams(match)) )
-  const view = new match.route.view(getParams(match));
+  const view = new match.view(getParams(match));
 
   // Apply the view to the DOM
   document.querySelector("#app").innerHTML = await view.getHtml();
