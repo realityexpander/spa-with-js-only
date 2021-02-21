4c12d1ae99f9ab1159cfc7e8eb1a8a87429d5305
Chris Athanas <chris@crowdskout.com>
2021-02-20 20:44:11 -0600

    primitive routing

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,30 @@
-console.log("JS is loaded frontend/static/js/index.js")
+const router = async () => {
+  const routes = [
+    { path: "/", view: () => console.log("Viewing Dashboard")},
+    { path: "/posts", view: () => console.log("Viewing posts")},
+    { path: "/settings", view: () => console.log("Viewing settings")}
+  ]
+
+  const potentialMatches = routes.map( route=> {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path
+    }
+  })
+
+  let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch)
+
+  // 404 route
+  if (!match) {
+    match = {
+      route: routes[0],
+      isMatch: true
+    }
+  }
+
+  console.log(match?.route?.view())
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  router();
+})
